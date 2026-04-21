26152333307820c50e7a57294d3732e632a58c5c
Izza <izzaadityasatriaw@gmail.com>
2026-04-21 23:05:19 +0700

    new caps4

--- belajar-ci/belajar-ci 1/app/Views/components/header.php
+++ belajar-ci/belajar-ci 1/app/Views/components/header.php
@@ -4,7 +4,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">IZZA AD</span>
+        <span class="d-none d-lg-block">JOVAN</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
@@ -170,7 +170,7 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2"><?= session()->get('username'); ?> (<?= session()->get('role'); ?>)</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2"> <?= session()->get('username'); ?> (<?= session()->get('role'); ?>) </span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
